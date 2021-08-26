4eaa2811cbadfe3ee2ea573a367f0c2a3f1bdf53
SimonLee <lixiaomin_235@163.com>
2021-08-26 13:03:57 +0800

    add Unreal project

---
+++ Unreal/Matrix/Source/Matrix/MatrixGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MatrixGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MATRIX_API AMatrixGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
